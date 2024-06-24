5a2e9ee588498fdf3b782b8917f6ba6e05489fad
Kyle <kdleonard@icloud.com>
2024-06-23 20:46:11 -0500

    Added a aggregate totals table.
    + a bunch of tests and the start of adding/deleting transactions

--- tracker/tests/conftest.py
+++ tracker/tests/conftest.py
@@ -1,8 +1,26 @@
 import pytest 
-from tracker.factories import TransactionFactory
+from tracker.factories import TransactionFactory, UserFactory
 
 
 @pytest.fixture
 def transactions():
-    import random
-    return TransactionFactory.create_batch(20, amount=random.randint(0,50))
+    return TransactionFactory.create_batch(20)
+
+@pytest.fixture
+def user_transactions():
+    user = UserFactory()
+    return TransactionFactory.create_batch(20, user=user)
+
+@pytest.fixture
+def user():
+    return UserFactory()
+
+@pytest.fixture
+def transaction_dict_params(user):
+    transaction = TransactionFactory.create(user=user)
+    return {
+        'type': transaction.type,
+        'category': transaction.category_id,
+        'date': transaction.date,
+        'amount': transaction.amount,
+    }

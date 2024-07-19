39f8a235d8a0fabcc5b5e3caf3e9f7d0918923dc
Kyle <kdleonard@icloud.com>
2024-07-18 20:58:48 -0500

    Fixing static files

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -140,9 +140,10 @@ USE_TZ = True
 if DEBUG:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 else:
     STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type

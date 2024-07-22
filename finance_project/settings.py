531eec770d5b5b533018ead4dc8414b7564314da
Kyle <kdleonard@icloud.com>
2024-07-22 08:02:53 -0500

    Removed uneeded CSS
    and changed DEBUG rule

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hfjjz86s+pz6y4p=&mxz)dis&l$+mt2)6r$ho@))^5))f#$whp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv('RENDER') else True
 
 ALLOWED_HOSTS = ['leo-ledger.onrender.com', 'localhost', '127.0.0.1']
 

268ee47b605e2220a7d82839c041e80f9878520f
Kyle <kdleonard@icloud.com>
2024-07-22 21:58:31 -0500

    🫠

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 from tracker import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,7 +12,6 @@ urlpatterns = [
     path('transactions/<int:pk>/update/', views.update_transaction, name='update-transaction'),
     path('transactions/<int:pk>/delete/', views.delete_transaction, name='delete-transaction'),
     path('get-transactions/', views.get_transactions, name='get-transactions'),
-    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:

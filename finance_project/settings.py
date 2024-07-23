4a22ea66cff3cf80f72d93dddaef676eb80da83b
Kyle <kdleonard@icloud.com>
2024-07-22 19:45:32 -0500

    lol yikes

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hfjjz86s+pz6y4p=&mxz)dis&l$+mt2)6r$ho@))^5))f#$whp"
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.getenv('RENDER') else True
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = ['leo-ledger.onrender.com', 'localhost', '127.0.0.1']
 
